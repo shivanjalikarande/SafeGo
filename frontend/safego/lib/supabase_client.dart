f1691645ec6d37768d4957829ea073566b8ccf40
Shivanjali Karande <145892931+shivanjalikarande@users.noreply.github.com>
2025-04-14 12:00:51 +0530

    Update supabase_client.dart

--- frontend/safego/lib/supabase_client.dart
+++ frontend/safego/lib/supabase_client.dart
@@ -1,6 +1,6 @@
 import 'package:supabase/supabase.dart';
 
 final supabase = SupabaseClient(
-  'https://kvdospgqfikkiymdsrmm.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2ZG9zcGdxZmlra2l5bWRzcm1tIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDAwODY3NiwiZXhwIjoyMDU5NTg0Njc2fQ.4Qu3C0sRXDfkYKpqZOXViqv8ElWs7Wh4W9maXLmKUiU',
+      --------------------,
+      -------------------
 );
