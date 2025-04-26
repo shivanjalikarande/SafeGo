8659d4cb0a26e5d478447aef8a66cc8abe6d4888
Shivanjali Karande <shivanjalikarande1@gmail.com>
2025-04-26 12:51:57 +0530

    Added SOS routes

--- backend/routes/sosRoutes.js
+++ backend/routes/sosRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { triggerSOS } = require('../controllers/sosController');
+const {getSOSHistory, addSOSHistory} = require('../controllers/sosController');
 
-router.post('/trigger-sos', triggerSOS);
+// router.post('/trigger-sos', triggerSOS);
+router.post('/', addSOSHistory);
+router.get('/:userId', getSOSHistory);
 
 module.exports = router;
